b2aa62bc470b5b30473c16c4aae355788d48e1ac
Fisher Chang <blue_fisher@qq.com>
2019-11-25 22:28:41 +0800

    怎加与服务器交互代码

--- battle.py
+++ battle.py
@@ -9,6 +9,9 @@ from player.mcts_rl import MCTS_POLICY, MCTSRL
 from utils.timer import timer
 from utils.sth import load_config
 
+import requests
+import time
+
 flags.DEFINE_integer('board_size', 37, '棋盘尺寸大小')
 flags.DEFINE_integer('box_size', 19, 'box的大小')
 flags.DEFINE_string('model_path', './models', '指定要加载的模型文件夹')
@@ -86,10 +89,38 @@ class HumanPlayer:
 
 
 class TestPlayer:
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, ip='127.0.0.1', port=8080, is_black=True):
+        self.url = f'http://{ip}:{port}'
+        self.color = 'BLACK' if is_black else 'WHITE'
+        r = requests.post(f'{self.url}/login', json={
+            'color': self.color
+        })
+        r = r.json()
+        if r['code'] == 200:
+            self.token = r['token']
+            print('注册成功', self.token)
+            self.last_x, self.last_y = [], []
+        else:
+            print('注册失败')
 
-    def choose_action(*args, **kwargs):
+    def move(self, x, y):  # x: [3, 4] y: [7, 8]
+        assert len(x) == len(y), 'x与y的长度必须相同'
+        movements = []
+        for i in range(len(x)):
+            movements.append({
+                'order': i + 1,
+                'color': self.color,
+                'x': x[i],
+                'y': y[i]
+            })
+
+        requests.post(f'{self.url}/move', headers={
+            'token': self.token
+        }, json={
+            'movements': movements
+        })
+
+    def choose_action(self):
         '''
         返回对手走的两步棋
         例如：
@@ -97,7 +128,27 @@ class TestPlayer:
             y: [7, 8]
         如果只走一步，返回: x: [3] y: [4]
         '''
-        return [0], [0]
+        while True:
+            r = requests.get(f'{self.url}/latest')
+            movements = r.json()['movements']
+
+            if len(movements) != 0 and movements[0]['color'] == self.color:
+                print('等待对方落子', movements)
+                time.sleep(1)
+                continue
+
+            tmp_x, tmp_y = [], []
+            for m in movements:
+                tmp_x.append(m['x'])
+                tmp_y.append(m['y'])
+
+            if tmp_x != self.last_x or tmp_y != self.last_y:
+                self.last_x, self.last_y = tmp_x, tmp_y
+                print('对方落子', movements)
+                return tmp_x, tmp_y
+
+            print('等待对方落子', movements)
+            time.sleep(1)
 
 
 if __name__ == '__main__':
@@ -105,3 +156,25 @@ if __name__ == '__main__':
         app.run(main)
     except SystemExit:
         pass
+
+
+# if __name__ == '__main__':
+#     import sys
+#     is_black = True
+#     if len(sys.argv[1:]) != 0:
+#         is_black = False
+#     print('BLACK' if is_black else 'WHITE')
+#     player = TestPlayer(is_black=is_black)
+#     if not is_black:
+#         x, y = player.choose_action()
+#         print('receive', x, y)
+#     while True:
+#         s = input()
+#         x, y = s.split(',')
+#         x, y = x.split(' '), y.split(' ')
+#         x = [int(i) for i in x]
+#         y = [int(i) for i in y]
+#         print(x, y)
+#         player.move(x, y)
+#         x, y = player.choose_action()
+#         print('receive', x, y)
