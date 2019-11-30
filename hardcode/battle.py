564ab0fad9b85f32aeed59b4bd350ea80b276948
Keavnn <271668153@qq.com>
2019-11-30 16:26:36 +0800

    自在极意v3

--- hardcode/battle.py
+++ hardcode/battle.py
@@ -323,8 +323,8 @@ class CounterPlayer(Base):
                             continue
                         if env.judge(x, y, self.flag, self.oppo_flag, 1):
                             return [x0, x], [y0, y]
-                        x1, y1 = partial_env1.get_next()
-                    return [x0, x1], [y0, y1]
+                    x1, y1 = partial_env1.get_next()
+                return [x0, x1], [y0, y1]
             return ret
 
     def update(self, env, x, y):
